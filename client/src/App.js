1273d1c54f58154ebf94b101cd680c884eaaaceb
VietThang269 <114757489+VietThang269@users.noreply.github.com>
2022-10-19 19:23:23 +0700

    Thang test thu commit - L1

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,27 @@
 import { Route, Switch } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Cart from "./components/Cart/Cart";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
 import Product from "./components/Product/Product";
 import Profile from "./components/Profile/Profile";
-import { AuthProvider } from "./contexts/AuthContext";import PrivateRoute from "./PrivateRoute";
-;
+import { AuthProvider } from "./contexts/AuthContext";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
   return (
     <div className="App">
-        <AuthProvider>
-          <Switch>
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-            <PrivateRoute exact path="/" component={Home} />
-            <Route path="/product" component={Product} />
-            <Route path="/cart" component={Cart} />
-            <Route path="/profile" component={Profile} />
-          </Switch>
-        </AuthProvider>
+      <AuthProvider>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <PrivateRoute exact path="/" component={Home} />
+          <Route path="/product" component={Product} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </AuthProvider>
     </div>
   );
 }
