08f8ffd8270e01f02c50f7832fc2dce4171ec3a8
VietThang269 <114757489+VietThang269@users.noreply.github.com>
2022-10-20 23:00:41 +0700

    Thang - Header - l1

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+import { Button } from "@mui/material";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header_logo">
+        <img src="" alt="" />
+        <a href="">NITC</a>
+      </div>
+      <div className="header_search"></div>
+      <div className="header_option"></div>
+    </header>
+  );
+};
+
+export default Header;
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,53 +1,62 @@
 import React, { useRef, useState } from "react";
 import { useAuth } from "../../contexts/AuthContext";
-import { useHistory  } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import Header from "../Header/Header";
 
 export default function Login() {
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const { login } = useAuth()
-  const [error, setError] = useState("")
-  const [loading, setLoading] = useState(false)
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const { login } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   const history = useHistory();
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
-      setError("")
-      setLoading(true)
-      login(emailRef.current.value, passwordRef.current.value)
-      console.log()
-      history.push("/")
+      setError("");
+      setLoading(true);
+      login(emailRef.current.value, passwordRef.current.value);
+      history.push("/");
     } catch {
-      setError("Failed to log in")
+      console.log("Hi");
+      setError("Failed to log in");
     }
 
-    setLoading(false)
+    setLoading(false);
   }
 
   return (
     <div>
-
-      <form className='login-form' onSubmit={handleSubmit}>
-        <span className='title'>Login an account</span>
-        {
-          error && <div className='error-msg'>{error}</div>
-        }
-        <div className='email'>
-            <span>Email</span>
-            <input ref={ emailRef } type="email" placeholder='Enter your email'></input>
+      <Header />
+      <form className="login-form" onSubmit={handleSubmit}>
+        <span className="title">Login an account</span>
+        {error && <div className="error-msg">{error}</div>}
+        <div className="email">
+          <span>Email</span>
+          <input
+            ref={emailRef}
+            type="email"
+            placeholder="Enter your email"
+          ></input>
         </div>
-        <div className='password'>
-            <span>Password</span>
-            <input ref={passwordRef} type="password" placeholder='Enter your password'></input>
+        <div className="password">
+          <span>Password</span>
+          <input
+            ref={passwordRef}
+            type="password"
+            placeholder="Enter your password"
+          ></input>
         </div>
 
-        <button disabled={loading} type='submit'>Log In</button>
-        <div className='signup-wrapper'> 
-            Need an account? <a href="/signup">Sign Up</a>
+        <button disabled={loading} type="submit">
+          Log In
+        </button>
+        <div className="signup-wrapper">
+          Need an account? <a href="/signup">Sign Up</a>
         </div>
       </form>
     </div>
-  )
+  );
 }
