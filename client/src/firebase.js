bc9d7ea9e37980e82787c8f86ac460cdab51b316
kohuyso <90167719+kohuyso@users.noreply.github.com>
2024-03-18 17:39:46 +0700

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,12 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyAS7F1rSRnwXT7Ymv-TBV8PNClnqo-98xA",
-    authDomain: "auth-neu-market.firebaseapp.com",
-    projectId: "auth-neu-market",
-    storageBucket: "auth-neu-market.appspot.com",
-    messagingSenderId: "766666736547",
-    appId: "1:766666736547:web:e31acc11be94a942591016"
+
 })
 
 export default app;
